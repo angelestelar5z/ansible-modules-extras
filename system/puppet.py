angelestelar5z/ansible-modules-extras

commit 2a44f79a1a9f33a11b7095fbd22f7dfa284cee27

fixed a few typos fixes #821

system/puppet.py
@@ -55,12 +55,12 @@ options:
     required: false
     default: None
   facter_basename:
-    desciption:
+    description:
       - Basename of the facter output file
     required: false
     default: ansible
   environment:
-    desciption:
+    description:
       - Puppet environment to be used.
     required: false
     default: None
@@ -161,9 +161,9 @@ def main():
                    base_cmd=base_cmd,
                    )
         if p['puppetmaster']:
-            cmd += " -- server %s" % pipes.quote(p['puppetmaster'])
+            cmd += " --server %s" % pipes.quote(p['puppetmaster'])
         if p['show_diff']:
-            cmd += " --show-diff"
+            cmd += " --show_diff"
         if p['environment']:
             cmd += " --environment '%s'" % p['environment']
         if module.check_mode:

commit c53a7056b6f85b8775ac46d2a2053c0390d022bb

Fix regression of #821 in puppet of show_diff
Issue #821 fix: 2a44f79a1a9f33a11b7095fbd22f7dfa284cee27
Regressed since: 1cbb83da46f76fbafe377405e92b7afd1c8b59a6

Issue: #821

system/puppet.py
@@ -175,7 +175,7 @@ def main():
         if p['puppetmaster']:
             cmd += " --server %s" % pipes.quote(p['puppetmaster'])
         if p['show_diff']:
-            cmd += " --show-diff"
+            cmd += " --show_diff"
         if p['environment']:
             cmd += " --environment '%s'" % p['environment']
         if module.check_mode: